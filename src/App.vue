b9839009a9d6be6aaac9d6f1d3d5ac9e15ae9a7c
nemo慢慢游 <ssc459610@gmail.com>
2026-01-26 22:20:37 +0800

    初始化系统布局

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="common-layout">
+    <el-container>
+      <el-aside
+        width="220px"
+        style="background-color: #545c64; min-height: 100vh"
+      >
+        <div
+          style="
+            height: 60px;
+            line-height: 60px;
+            text-align: center;
+            color: white;
+            font-weight: bold;
+            background-color: #434a50;
+          "
+        >
+          实验室管理系统
+        </div>
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          default-active="1"
+        >
+          <el-menu-item-group title="核心业务">
+            <el-menu-item index="1">
+              <el-icon><Monitor /></el-icon>
+              <span>资产全生命周期</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <el-icon><List /></el-icon>
+              <span>借用与归还</span>
+            </el-menu-item>
+          </el-menu-item-group>
+
+          <el-menu-item-group title="系统维护">
+            <el-menu-item index="3" disabled>
+              <el-icon><PieChart /></el-icon>
+              <span>数据统计 (开发中)</span>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header
+          style="
+            text-align: right;
+            font-size: 12px;
+            border-bottom: 1px solid #eee;
+            line-height: 60px;
+          "
+        >
+          <span>管理员：User</span>
+        </el-header>
+        <el-main>
+          <el-empty description="请点击左侧菜单选择功能" />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+import { Monitor, List, PieChart } from "@element-plus/icons-vue";
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
